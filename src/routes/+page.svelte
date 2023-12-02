299e05409dbaf0ba33c1ec7499f9225a4c95c8dd
Shishira <shishiraiyar@gmail.com>
2023-12-02 20:58:45 +0530

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    let imgUrl = 'src/lib/assets/bg-img.jpeg';
+    //Changing this to 'src/lib/assets/bg img.jpeg' breaks it.
+</script>
+
+<div class="back" style="background-image: url({imgUrl});">
+    <h1>Hello world!</h1>
+</div>
+
+<style>
+    .back{
+        min-height:100vh;
+    }
+</style>
